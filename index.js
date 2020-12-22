4c3213d4d30f479e8db72c2f3f3ae442129e6346
ArjunMenon <menonnujra@gmail.com>
2020-12-22 22:36:12 +0530

    cli app

---
+++ index.js
@@ -0,0 +1,11 @@
+const fs = require('fs');
+
+module.exports = html5
+
+function html5() {
+
+    const html5 = fs.readFileSync(`${__dirname}/index.html`)
+    fs.writeFileSync(`${process.cwd()}/index.html`, html5)
+}
+
+html5()
