24967f09957bbbd735469220366e10edfd19b155
ArjunMenon <menonnujra@gmail.com>
2020-12-23 00:40:12 +0530

    basic iterations

--- index.js
+++ index.js
@@ -1,11 +1,43 @@
-const csv = require('csv-parser');
-const fs = require('fs');
+var myObj, i, j, x = "",
+    y = [];
+myObj = {
+    "lift": [{
+        "at_level": 4,
+        "id": 1
+    }],
+    "passenger": [{
+            "at_level": 1,
+            "goto_level": 10,
+            "id": 1
+        },
+        {
+            "at_level": 1,
+            "goto_level": 24,
+            "id": 13
+        },
+        {
+            "at_level": 1,
+            "goto_level": 8,
+            "id": 18
+        }
+    ]
+}
+for (i in myObj.passenger) {
+    x = myObj.passenger[i].goto_level;
+    // console.log(x);
+    y.push(x);
+}
+y.sort(function(a, b) {
+    return a - b;
+});
+console.log(y);
 
-fs.createReadStream('passengers.csv')
-    .pipe(csv())
-    .on('data', (row) => {
-        console.log(row);
-    })
-    .on('end', () => {
-        console.log('CSV file successfully processed');
-    });
+for (j in myObj.lift) {
+    x = myObj.lift[j].at_level;
+
+    if (x != 1) {
+        console.log("GO DOWN");
+    } else {
+        console.log(`lift at ${x}`);
+    }
+}
