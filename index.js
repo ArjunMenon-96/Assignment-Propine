6ed4df89f40561df1f258ecd4f5432a3ca09d21a
ArjunMenon <menonnujra@gmail.com>
2020-12-24 21:50:05 +0530

    logic implementation complete

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ var futureStops = [];
 
 var instructions = [];
 
-
+var totalMax, totalMin, maxAt, minAt, maxGo, minGo;
 
 var myObj = {};
 passGo = [];
@@ -27,7 +27,8 @@ passAt = [];
 
 async function f() {
     // console.log("test9");
-    return fs.promises.readFile('./states/state_3.hcl').catch((err) => console.error('Failed to read file', err));
+    // return fs.promises.readFile('./states/state_10.hcl').catch((err) => console.error('Failed to read file', err));
+    return fs.promises.readFile(process.argv[2]).catch((err) => console.error('Failed to read file', err));
 }
 
 // console.log("test0");
@@ -50,7 +51,7 @@ f()
                 // console.log(x);
             passGo.push(q);
         }
-        console.log("passGo", passGo);
+        // console.log("passGo", passGo);
 
         for (var i = 0; i < myObj.passenger.length; i++) { // loop to get passAt
             var q = {
@@ -61,25 +62,47 @@ f()
                 // console.log(x);
             passAt.push(q);
         }
-        console.log("passAt", passAt);
+        // console.log("passAt", passAt);
 
 
         for (var j = 0; j < myObj.lift.length; j++) {
             liftAt = myObj.lift[j].at_level;
         }
-        console.log("liftAt", liftAt);
+        // console.log("liftAt", liftAt);
+
+        minAt = passAt[0].at_level;
+        maxAt = passAt[0].at_level;
+        minGo = passAt[0].goto_level;
+        maxGo = passAt[0].goto_level;
+
+        // passAt.map(a => a.at_level);
+
+        for (var k = 1; k < passAt.length; k++) {
+            if (passAt[k].at_level > maxAt) maxAt = passAt[k].at_level;
+            else if (passAt[k].at_level < minAt) minAt = passAt[k].at_level;
+            if (passAt[k].goto_level > maxGo) maxGo = passAt[k].goto_level;
+            else if (passAt[k].goto_level < minGo) minGo = passAt[k].goto_level;
+        }
+
+        // console.log("minAt " + minAt + " maxAt " + maxAt + " minGo " + minGo + " maxGo " + maxGo);
+
+        totalMax = (maxAt > maxGo) ? maxAt : maxGo;
+        totalMin = (minAt < minGo) ? minAt : minGo;
+
+        // console.log("totalMax " + totalMax + " totalMin " + totalMin);
 
         // console.log("test2");
 
-    }).then(function() { // algo
+    })
+    .then(function() { // algo
 
         // console.log("test3");
 
         // find nearest passenger
         passAt.sort(function(a, b) {
-            return Math.abs((a.at_level - liftAt)) - Math.abs((b.at_level - liftAt));
-        })
-        console.log("sorted passAt", passAt);
+                return Math.abs((a.at_level - liftAt)) - Math.abs((b.at_level - liftAt));
+            })
+            // console.log("sorted passAt", passAt);
 
         // futureStops = passAt.map(function (x) {return x.at_level});
         // futureStops.sort(function(a, b){return a-b});
@@ -90,41 +113,77 @@ f()
         else liftDir = "DOWN";
         // include exception of passenger at lift level
 
-        instructions.push("GO " + liftDir);
-        console.log("INSTRUCTIONS", instructions);
+        // instructions.push("GO " + liftDir);
+        // console.log("INSTRUCTIONS", instructions);
 
         //fake data
         //liftDir = "DOWN";
         while (true) {
 
+            // console.log("new loop, lift At: " + liftAt);
+
+            // Offload passengers at current level
+            var xx = 0;
+            while (xx < currPass.length) {
+                if (currPass[xx].goto_level == liftAt) {
+                    instructions.push("PASSENGER " + currPass[xx].id + " LEAVE");
+                    currPass.splice(xx, 1);
+                    // console.log("INSTRUCTIONS", instructions);
+                    continue;
+                }
+                xx++;
+            }
+
+            // Load passengers at current level
+            var xy = 0;
+            while (xy < passAt.length) {
+                if ((passAt[xy].at_level == liftAt) && (currPass.length < passLimit)) {
+                    // if ( (passAt[xy].at_level == liftAt) ) {
+                    currPass.push(passAt[xy]);
+                    currPass.sort(function(a, b) { return a.goto_level - b.goto_level });
+                    instructions.push("PASSENGER " + passAt[xy].id + " ENTER");
+                    passAt.splice(xy, 1);
+                    // console.log("INSTRUCTIONS", instructions);
+
+                    continue;
+                }
+                xy++;
+            }
+
+            // console.log("new currPass: ", currPass);
+
             // find next stop
-            console.log("new loop, lift At: " + liftAt);
             var nextStop = -1;
             var nextAction = ""; // pick, drop, both
 
             if ((passAt.length == 0) && (currPass.length == 0)) { // endLift;
                 // endLift();
-                console.log("endlift");
+                // console.log("endlift");
                 nextStop = 0;
                 nextAction = "end";
+                if (liftAt != 1) {
+                    instructions.push("GO DOWN");
+                    instructions.push("STOP 1");
+                }
                 break;
             } else if (currPass.length == passLimit) { // full Lift
                 // check closest drop off in same direction, else change direction
-                console.log("full lift");
+                // console.log("full lift");
+                // console.log("full next stop", nextStop, liftDir);
                 nextAction = "drop";
                 if (liftDir == "DOWN") {
                     for (var x = currPass.length - 1; x >= 0; x--) {
                         if (currPass[x].goto_level < liftAt) {
                             nextStop = currPass[x].goto_level;
                             instructions.push("GO DOWN");
-                            console.log(instructions);
+                            // console.log(instructions);
                             break;
                         }
                     }
                     if (nextStop == -1) {
                         liftDir = "UP";
                         instructions.push("GO UP");
-                        console.log(instructions);
+                        // console.log(instructions);
                         for (var x = 0; x < currPass.length; x++) {
                             if (currPass[x].goto_level > liftAt) {
                                 nextStop = currPass[x].goto_level;
@@ -138,14 +197,14 @@ f()
                         if (currPass[x].goto_level > liftAt) {
                             nextStop = currPass[x].goto_level;
                             instructions.push("GO UP");
-                            console.log(instructions);
+                            // console.log(instructions);
                             break;
                         }
                     }
                     if (nextStop == -1) {
                         liftDir = "DOWN";
                         instructions.push("GO DOWN");
-                        console.log(instructions);
+                        // console.log(instructions);
                         for (var x = currPass.length - 1; x >= 0; x--) {
                             if (currPass[x].goto_level < liftAt) {
                                 nextStop = currPass[x].goto_level;
@@ -156,52 +215,83 @@ f()
                 }
             } else if (currPass.length == 0) { // empty Lift
                 // go to closest passenger in same direction and pick
-                console.log("empty lift");
+                // console.log("empty lift");
+                // console.log("empty next stop", nextStop, liftDir);
                 nextAction = "pick";
                 if (liftDir == "UP") {
-                    for (var x = 0; x < passAt.length; x++) {
-                        if (passAt[x].at_level > liftAt) {
-                            nextStop = passAt[x].at_level;
-                            instructions.push("GO UP");
-                            console.log(instructions);
-                            break;
+                    var closest1 = totalMax;
+                    var flag1 = 0;
+                    for (var x = 0; x < passAt.length; x++) { // loop to find closest upper level
+                        if ((passAt[x].at_level > liftAt) && (passAt[x].at_level < closest1)) {
+                            // nextStop = passAt[x].at_level;
+                            closest1 = passAt[x].at_level;
+                            flag1 = 1;
+                            // instructions.push("GO UP");
+                            // console.log(instructions);
+                            // break;
                         }
                     }
-                    if (nextStop == -1) {
+                    if (flag1 != 0) { // if an upper floor is found, add to instructions
+                        nextStop = closest1;
+                        instructions.push("GO UP");
+                        // console.log(instructions);
+                    }
+
+                    if (nextStop == -1) { // if no upper floor found
                         liftDir = "DOWN";
                         instructions.push("GO DOWN");
-                        console.log(instructions);
-                        for (var x = passAt.length - 1; x >= 0; x--) {
-                            if (passAt[x].at_level < liftAt) {
-                                nextStop = passAt[x].at_level;
-                                break;
+                        // console.log(instructions);
+                        // console.log("passAt", passAt);
+                        var closest2 = -1;
+                        // var cIndex = -1;
+                        for (var x = passAt.length - 1; x >= 0; x--) { // loop to find closest lower level
+                            if ((passAt[x].at_level < liftAt) && (passAt[x].at_level > closest2)) {
+                                // nextStop = passAt[x].at_level;
+                                closest2 = passAt[x].at_level;
+                                // cIndex = x;
+                                // break;
                             }
                         }
+                        nextStop = closest2;
                     }
                 } else if (liftDir == "DOWN") {
-                    for (var x = passAt.length - 1; x >= 0; x--) {
-                        if (passAt[x].at_level < liftAt) {
-                            nextStop = passAt[x].at_level;
-                            instructions.push("GO DOWN");
-                            console.log(instructions);
-                            break;
+                    var closest3 = -1;
+                    var flag3 = 0;
+                    for (var x = passAt.length - 1; x >= 0; x--) { // loop to find closest lower level
+                        if ((passAt[x].at_level < liftAt) && (passAt[x].at_level > closest3)) {
+                            // nextStop = passAt[x].at_level;
+                            closest3 = passAt[x].at_level;
+                            flag3 = 1;
+                            // instructions.push("GO DOWN");
+                            // console.log(instructions);
+                            // break;
                         }
                     }
-                    if (nextStop == -1) {
+                    if (flag3 != 0) { // if a lower floor is found, add to instructions
+                        nextStop = closest3;
+                        instructions.push("GO DOWN");
+                        // console.log(instructions);
+                    }
+                    if (nextStop == -1) { // if no lower level found
                         liftDir == "UP";
                         instructions.push("GO UP");
-                        console.log(instructions);
+                        // console.log(instructions);
+                        // console.log("passAt", passAt);
+                        var closest4 = totalMax;
                         for (var x = 0; x < passAt.length; x++) {
-                            if (passAt[x].at_level > liftAt) {
-                                nextStop = passAt[x].at_level;
-                                break;
+                            if ((passAt[x].at_level > liftAt) && (passAt[x].at_level < closest4)) {
+                                // nextStop = passAt[x].at_level;
+                                // break;
+                                closest4 = passAt[x].at_length;
                             }
                         }
+                        nextStop = closest4;
                     }
                 }
             } else { // normal condition, keep going in the same direction until nothing left
                 // curr Pass dropoff + pass to be picked, closest in the same direction
-                console.log("norm lift");
+                // console.log("norm lift");
+                // console.log("norm next stop", nextStop, liftDir);
                 var newArr = [];
                 for (var i = 0; i < currPass.length; i++)
                     newArr.push({
@@ -217,14 +307,14 @@ f()
                     })
 
                 newArr.sort(function(a, b) { return a.level - b.level });
-                console.log("newArr", newArr)
+                // console.log("newArr", newArr)
 
                 if (liftDir == "UP") {
                     for (var x = 0; x < newArr.length; x++) {
                         if (newArr[x].level > liftAt) {
                             nextStop = newArr[x].level;
                             instructions.push("GO UP");
-                            console.log(instructions);
+                            // console.log(instructions);
                             nextAction = newArr[x].type;
                             break;
                         }
@@ -232,7 +322,7 @@ f()
                     if (nextStop == -1) {
                         liftDir = "DOWN";
                         instructions.push("GO DOWN");
-                        console.log(instructions);
+                        // console.log(instructions);
                         for (var x = newArr.length - 1; x >= 0; x--) {
                             if (newArr[x].level < liftAt) {
                                 nextStop = newArr[x].level;
@@ -246,7 +336,7 @@ f()
                         if (newArr[x].level < liftAt) {
                             nextStop = newArr[x].level;
                             instructions.push("GO DOWN");
-                            console.log(instructions);
+                            // console.log(instructions);
                             nextAction = newArr[x].type;
                             break;
                         }
@@ -254,7 +344,7 @@ f()
                     if (nextStop == -1) {
                         liftDir == "UP";
                         instructions.push("GO UP");
-                        console.log(instructions);
+                        // console.log(instructions);
                         for (var x = 0; x < newArr.length; x++) {
                             if (newArr[x].level > liftAt) {
                                 nextStop = newArr[x].level;
@@ -267,12 +357,11 @@ f()
 
             }
 
-            console.log("next stop: ", nextStop)
+            // console.log("next stop: ", nextStop)
             instructions.push("STOP " + nextStop);
-            console.log("INSTRUCTIONS", instructions);
+            // console.log("INSTRUCTIONS", instructions);
             liftAt = nextStop;
-
-            console.log("lift at " + liftAt + "   nextAction " + nextAction);
+            // console.log("lift at " + liftAt + "  nextAction " + nextAction);
 
             // if (nextAction == "drop") {
             //     var x = 0;
@@ -306,32 +395,33 @@ f()
 
             // }
 
-            var xx = 0;
-            while (xx < currPass.length) {
-                if (currPass[xx].goto_level == liftAt) {
-                    instructions.push("PASSENGER " + currPass[xx].id + " LEAVE");
-                    currPass.splice(xx, 1);
-                    console.log("INSTRUCTIONS", instructions);
-                    continue;
-                }
-                xx++;
-            }
+            // var xx = 0;
+            // while (xx < currPass.length) {
+            //     if (currPass[xx].goto_level == liftAt) {
+            //         instructions.push("PASSENGER "+currPass[xx].id+ " LEAVE");
+            //         currPass.splice(xx, 1);
+            //         console.log("INSTRUCTIONS", instructions);
+            //         continue;
+            //     }
+            //     xx++;
+            // }
 
-            var xy = 0;
-            while (xy < passAt.length) {
-                if (passAt[xy].at_level == liftAt) {
-                    currPass.push(passAt[xy]);
-                    currPass.sort(function(a, b) { return a.goto_level - b.goto_level });
-                    instructions.push("PASSENGER " + passAt[xy].id + " ENTER");
-                    passAt.splice(xy, 1);
-                    console.log("INSTRUCTIONS", instructions);
+            // var xy = 0;
+            // while (xy < passAt.length) {
+            //     if ( (passAt[xy].at_level == liftAt) && (currPass.length < passLimit) ) {
+            //     // if ( (passAt[xy].at_level == liftAt) ) {
+            //         currPass.push(passAt[xy]);
+            //         currPass.sort(function(a, b){return a.goto_level-b.goto_level});
+            //         instructions.push("PASSENGER "+passAt[xy].id+ " ENTER");
+            //         passAt.splice(xy, 1);
+            //         console.log("INSTRUCTIONS", instructions);
 
-                    continue;
-                }
-                xy++;
-            }
+            //         continue;
+            //     }
+            //     xy++;
+            // }
 
-            console.log("new currPass: ", currPass);
+            // console.log("new currPass: ", currPass);
 
 
 
@@ -343,6 +433,8 @@ f()
             // }
         }
 
+
+        // console.log("argv", process.argv);
         console.log("INSTRUCTIONS", instructions);
 
 
